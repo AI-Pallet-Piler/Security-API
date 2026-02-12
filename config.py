88c8210e1812ad558e23c9c2840cc6e02c6c200a
dominium2 <danial.mubeasher@student.ehb.be>
2026-02-12 12:56:05 +0100

    remove env_file configuration from model settings

--- config.py
+++ config.py
@@ -37,7 +37,6 @@ class Settings(BaseSettings):
     log_level: str = "INFO"
     
     model_config = SettingsConfigDict(
-        env_file=".env",
         env_file_encoding="utf-8",
         case_sensitive=False
     )
